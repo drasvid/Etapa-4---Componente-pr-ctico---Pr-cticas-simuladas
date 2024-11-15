12d04d1d7456c33897bf3badbef763b44080c5c4
Drasvid <da70699@outlook.com>
2024-11-15 04:04:54 -0500

    first commit

---
+++ Trabajo 4/Program.cs
@@ -0,0 +1,114 @@
+﻿int[] numbers = new int[10];
+int eleccion;
+string[] options = ["Bubble", "Shell", "Seleccion", "Interseccion"];
+
+Organizer organizer = new Organizer();
+
+do
+{
+
+
+    eleccion = organizer.opciones();
+
+
+
+
+
+
+
+    Console.WriteLine("Inserte los 10 digitos del arreglo: ");
+
+
+    for (int i = 0; i <= 9; i++)
+    {
+        Console.WriteLine("Inserte el digito: " + (i + 1));
+
+        int digit = int.Parse(Console.ReadLine());
+
+
+        numbers[i] = digit;
+    }
+
+
+    //////////////////////////////////////////////////////////////////
+
+
+    switch (eleccion)
+    {
+
+        case 1:
+
+            organizer.bubbleOrder(numbers);
+
+            break;
+
+        case 2:
+
+            organizer.ShellSort(numbers);
+
+            break;
+        case 3:
+
+            organizer.seleccion(numbers);
+
+
+            break;
+        case 4:
+
+            organizer.Insercion(numbers);
+
+
+            break;
+
+        case 5:
+
+
+
+            Console.WriteLine("Elija el primer algoritmo");
+            eleccion = organizer.opciones_1();
+            double valor_1 = organizer.swith(eleccion, numbers);
+            Console.WriteLine("Elija el segundo algoritmo");
+            int eleccion_2 = organizer.opciones_1();
+            double valor_2 = organizer.swith(eleccion_2, numbers);
+
+            Console.WriteLine($"El tiempo del algoritmo {options[(eleccion - 1)]} es {valor_1} y el del algoritmo {options[(eleccion_2 - 1)]} es {valor_2}");
+
+            if (valor_1 < valor_2)
+            {
+
+
+                double porcentaje = (valor_1 * 100) / valor_2;
+
+                Console.WriteLine(porcentaje);
+
+                Console.WriteLine($"De acuerdo con la info recolectada, podemos ver que el algoritmo {options[(eleccion - 1)]} es un {100-porcentaje}% mas rapido que el algoritmo {options[(eleccion_2 - 1)]} en este caso especifico");
+
+
+            }
+            else
+            {
+
+
+                double porcentaje = (valor_2 * 100) / valor_1;
+                Console.WriteLine(porcentaje);
+
+                Console.WriteLine($"De acuerdo con la info recolectada, podemos ver que el algoritmo {options[(eleccion_2 - 1)]} es un {100-porcentaje}% mas rapido que el algoritmo {options[(eleccion - 1)]} en este caso especifico");
+
+
+            }
+
+
+            break;
+
+
+
+
+
+    }
+
+
+
+
+
+}
+while (eleccion>0);
---
+++ Trabajo 4/clases/organizer.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+public class Organizer
+{
+
+
+    public double swith(int eleccion, int[] numbers) {
+
+
+        switch (eleccion)
+        {
+
+            case 1:
+
+                return bubbleOrder(numbers);
+
+                break;
+
+            case 2:
+
+                return ShellSort(numbers);
+
+                break;
+            case 3:
+
+                return seleccion(numbers);
+
+
+                break;
+            case 4:
+
+                return Insercion(numbers);
+
+
+                break;
+
+        }
+
+
+        return 1.2;
+    }
+    public int opciones() {
+
+        int eleccion = 0;
+        string msg = "";
+
+
+        Console.WriteLine("1. Ordenamiento de Burbuja");
+        Console.WriteLine("2. Ordenamiento de Shell");
+        Console.WriteLine("3. Ordenamiento de Seleccion");
+        Console.WriteLine("4. Ordenamiento de Insercion");
+        Console.WriteLine("5. Benchmark entre algoritmos");
+        Console.WriteLine("0. Salir");
+
+
+        do
+        {
+            Console.WriteLine("Elija el metodo de ordenamientoa utilizar: ");
+
+
+
+            try
+            {
+
+
+                eleccion = int.Parse(Console.ReadLine());
+
+
+
+            }
+            catch (Exception e)
+            {
+
+                msg = e.Message;
+
+                Console.WriteLine("Opcion no valida");
+
+
+            }
+
+
+        }
+        while (msg.Length > 0);
+
+
+        return eleccion;
+
+    }
+
+    public int opciones_1()
+    {
+
+        int eleccion = 0;
+        string msg = "";
+
+
+        Console.WriteLine("1. Ordenamiento de Burbuja");
+        Console.WriteLine("2. Ordenamiento de Shell");
+        Console.WriteLine("3. Ordenamiento de Seleccion");
+        Console.WriteLine("4. Ordenamiento de Insercion");
+
+        do
+        {
+
+
+
+            try
+            {
+
+
+                eleccion = int.Parse(Console.ReadLine());
+
+
+
+            }
+            catch (Exception e)
+            {
+
+                msg = e.Message;
+
+                Console.WriteLine("Opcion no valida");
+
+
+            }
+
+
+        }
+        while (msg.Length > 0);
+
+
+        return eleccion;
+
+    }
+
+
+    int[] numbers;
+    DateTime date=DateTime.Now;
+
+
+    private double tiempo(DateTime date) {
+
+        double time;
+
+
+        return  time=DateTime.Now.Subtract(date).Milliseconds;
+
+
+
+    }
+
+
+
+    public double bubbleOrder(int[] numbers ) {
+
+        int counter = 0;
+
+        DateTime date = DateTime.Now;
+
+        do
+        {
+
+            for (global::System.Int32 i = 0; i <= 9; i++)
+            {
+                if (i<9)
+                {
+
+                    int x = numbers[i];
+                    int y = numbers[i + 1];
+
+
+                    if (x > y)
+                    {
+
+
+                        numbers[i] = y;
+                        numbers[i + 1] = x;
+
+
+
+
+                    }
+
+
+
+                }
+
+            }
+            counter++;
+
+        }
+        while (counter<=8);
+
+        Console.WriteLine("El arreglo ordenado seria");
+
+        foreach (var item in numbers)
+        {
+            Console.WriteLine(item);
+            
+        }
+
+
+
+        double info_time=tiempo(date);
+
+        Console.WriteLine($"El tiempo que tomo el algoritmo en organizar la info fue: {info_time} milisegundos");
+
+        return info_time;   
+        
+
+
+    }
+
+
+    public double ShellSort(int[] array)
+    {
+
+        DateTime date = DateTime.Now;
+
+        int n = array.Length;
+        int gap = n / 2;
+
+        while (gap > 0)
+        {
+            for (int i = gap; i < n; i++)
+            {
+                int temp = array[i];
+                int j = i;
+
+                while (j >= gap && array[j - gap] > temp)
+                {
+                    array[j] = array[j - gap];
+                    j -= gap;
+                }
+
+                array[j] = temp;
+            }   
+
+            gap /= 2;
+        }
+
+        Console.WriteLine("El arreglo ordenado seria");
+
+
+        foreach (var item in array)
+        {
+
+
+            Console.WriteLine($"{item}");
+        }
+
+        double info_time = tiempo(date);
+
+        Console.WriteLine($"El tiempo que tomo el algoritmo en organizar la info fue: {info_time}  milisegundos");
+
+        return info_time;
+
+    }
+
+
+
+
+    public double seleccion(int[] numbers) {
+
+        DateTime date = DateTime.Now;
+
+
+        int x =0;
+        int y=0;
+
+        int[] ordenados = new int[numbers.Length];
+
+        for (int j = 0; j < numbers.Length; j++)
+        {
+
+            for (int i = (j+1); i < numbers.Length; i++)
+            {
+
+                if (numbers[i] < numbers[j])
+                {
+                    x = i;
+
+                    y = numbers[j];
+
+                    numbers[j] = numbers[x];
+                    numbers[x] = y;
+
+                }
+                else { 
+                
+                    x= j;
+     
+
+                }
+
+
+
+
+            }
+
+
+        }
+
+
+        Console.WriteLine("El arreglo ordenado seria");
+
+
+        foreach (var item in numbers)
+        {
+
+            Console.WriteLine(item);
+            
+        }
+
+
+        double info_time = tiempo(date);
+
+        Console.WriteLine($"El tiempo que tomo el algoritmo en organizar la info fue: {info_time} milisegundos");
+
+        return info_time;
+
+
+    }
+
+
+
+    public double Insercion(int[] numbers) {
+
+        DateTime date = DateTime.Now;
+
+
+        int x = 0;
+        int y = 0;
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+
+
+            for (int j = (i-1); j >= 0; j--)
+            {
+
+                //Console.WriteLine($"j es igual a: {j}");
+
+                //Console.WriteLine($"Es {numbers[i]} menor que {numbers[j]}");
+
+
+                if (numbers[i] < numbers[j])
+                {
+
+                    //Console.WriteLine($"SI, {numbers[i]} menor que {numbers[j]}");
+
+                    x = j;
+
+                    y = numbers[i];
+
+                    numbers[i] = numbers[x];
+                    numbers[x] = y;
+
+
+                    i--;
+
+
+                }
+                else
+                {
+
+
+                    x = i;
+
+                    //Console.WriteLine($"NO, {numbers[i]} no es menor que {numbers[j]}");
+
+                }
+
+                //Console.WriteLine($"Arreglo parcial");
+                //foreach (var item in numbers) { 
+                
+                //    Console.WriteLine(item);
+                
+                //}
+
+
+            }
+
+        }
+
+        Console.WriteLine("El arreglo ordenado seria");
+
+
+        foreach (var item in numbers)
+        {
+
+            Console.WriteLine(item);
+
+        }
+
+
+        double info_time = tiempo(date);
+
+        Console.WriteLine($"El tiempo que tomo el algoritmo en organizar la info fue: {info_time} milisegundos");
+
+        return info_time;
+
+
+    }
+
+
+
+}
